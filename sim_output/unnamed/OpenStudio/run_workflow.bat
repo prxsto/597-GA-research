bcc276245a0b71eeef23903b86f9a4a9498aca7f
preston pape <papepreston@gmail.com>
2021-06-01 16:10:39 -0700

    testing to find fix to E+ sim

---
+++ sim_output/unnamed/OpenStudio/run_workflow.bat
@@ -0,0 +1,2 @@
+C:
+"C:\openstudio-3.1.0\bin\openstudio.exe" -I "C:\Users\papep\ladybug_tools\resources\measures\honeybee_openstudio_gem\lib" run -m -w "C:\Users\papep\Documents\GitHub\597-GA-research\sim_output\unnamed\OpenStudio\workflow.osw"
